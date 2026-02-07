e85414d728990dfe9199e9cb8f12c922ac0cb035
wongpiwat <wongpiwat.san@gmail.com>
2026-02-06 16:32:25 -0800

    feat: allowed the render hosts to tun the project

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["class-compass.onrender.com"],
     hmr: {
       overlay: false,
     },
